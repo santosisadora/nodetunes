caf4c194a8140029797db5e3f0c7953142f0b366
Isadora-Santos1 <isadora.sass@gmail.com>
2021-10-20 09:51:13 -0400

    Connected application to MongoDB

---
+++ config/globals.js
@@ -0,0 +1,4 @@
+//global config vars
+module.exports = {
+    "db": "mongodb+srv://db1:isadora@cluster0.rm1yo.mongodb.net/nodedb"
+}
--- controllers/artists.js
+++ controllers/artists.js
@@ -4,8 +4,48 @@ let express = require ('express')
 //instantiate an express router to parse and direct url requests
 let router = express.Router()
 
-router.get('/',(req,res)=>{
-    res.render('artists/index')
+//add model ref
+const Artist = require('../models/artist')
+
+router.get('/',(req,res) => {
+    //use Artist model to fetch all documents from artists collection in mongodb
+    Artist.find((err,artists) =>{
+        if(err){
+            console.log(err)
+            res.end(err)
+        }
+        else{
+            console.log(artists)
+            res.render('artists/index',{
+                artists:artists
+            })
+        }
+    })
+
+ //   res.render('artists/index')
+})
+
+// GET: artists/create => show new artist form
+router.get('/create',(req,res)=>{
+    res.render('artists/create',{
+        title:'Add a new Artist'
+    })
+})
+
+// POST: artists/create => process form submission & save new Artist document
+router.post('/create',(req,res)=>{
+  //use mongoose model to create a new Artist document
+    Artist.create({
+        name: req.body.name
+    },(err, newArtist)=>{
+        if(err){
+            console.log(err)
+            res.end(err)
+        }
+        else{//save successful > update artists list view
+            res.redirect('/artists')
+        }
+    })
 })
 
 //make public
